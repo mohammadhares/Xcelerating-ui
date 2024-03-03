a037bfb5d38a61710a2145290db18c08bbab19eb
Haris <mharis@tibxtech.com>
2024-03-03 22:26:59 +0430

    Intial Commit

---
+++ src/components/Blog/index.tsx
@@ -0,0 +1,29 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleBlog from "./SingleBlog";
+import blogData from "./blogData";
+
+const Blog = () => {
+  return (
+    <section
+      id="blog"
+      className="bg-gray-light dark:bg-bg-color-dark py-16 md:py-20 lg:py-28"
+    >
+      <div className="container">
+        <SectionTitle
+          title="Our Latest Blogs"
+          paragraph="Stay informed and inspired with Our Latest Blogs, your gateway to insightful articles on industry trends, expert insights, and practical tips for personal and professional growth."
+          center
+        />
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
+          {blogData.map((blog) => (
+            <div key={blog.id} className="w-full">
+              <SingleBlog blog={blog} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
---
+++ src/components/Brands/brandsData.tsx
@@ -0,0 +1,48 @@
+import { Brand } from "@/types/brand";
+
+const brandsData: Brand[] = [
+  {
+    id: 1,
+    name: "Microsoft",
+    href: "https://microsoft.com",
+    image: "",
+    imageLight: "",
+  },
+  {
+    id: 2,
+    name: "Meta Tech",
+    href: "https://meta.com",
+    image: "/",
+    imageLight: "",
+  },
+  {
+    id: 3,
+    name: "MicroTech",
+    href: "https://microtech.com",
+    image: "",
+    imageLight: "",
+  },
+  {
+    id: 1,
+    name: "Cisco Services",
+    href: "https://cisco.com",
+    image: "",
+    imageLight: "",
+  },
+  {
+    id: 2,
+    name: "Nano Tech",
+    href: "https://nano.com",
+    image: "/",
+    imageLight: "",
+  },
+  {
+    id: 3,
+    name: "Wama Solution",
+    href: "https://wamasolution.com",
+    image: "",
+    imageLight: "",
+  }
+];
+
+export default brandsData;
