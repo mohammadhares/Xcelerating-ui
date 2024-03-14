f40e9797d20d15e84f04e979c40a4ac412a45e86
AliNazery <101720872+AliNazery@users.noreply.github.com>
2024-03-14 13:48:24 +0430

    new change to git push

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,8 +14,8 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="About Us"
+        description="discription."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
