434a74808f526c965e3831ef626a976bd4954546
Ali Nazery <101720872+AliNazery@users.noreply.github.com>
2024-03-15 01:06:11 +0430

    Add files via upload

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,8 +14,8 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="About Us"
+        description="Empowering institutions and governments to transcend limits, streamlining success with simplicity and maximizing savings."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
